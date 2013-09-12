e31d3ddc8f5697f9f9e569574f896684b1ee818e
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-09-12 14:47:09 +0200

    Remove uneccessary HTML, JS, and images
    Cleanup crew to the rescue. :)

--- _disabled/head_print.php
+++ _disabled/head_print.php
@@ -3,7 +3,7 @@
 <title>Welcome To Med Tech Sweden, Inc.</title>
 <meta charset="UTF-8"><meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <link rel="STYLESHEET" type="text/css" href="scripts/style.css">
-<script src="scripts/scripts.js" type="text/javascript"></script>
+<script src="js/main.js" type="text/javascript"></script>
 
 </head>
 <body>
